50e918dd89f1832c167dbfa40bfa167542116143
Vivek Gupta <vivek.gupta@wdc.com>
2022-09-24 16:38:53 +0530

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1 className="text-5xl">Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
